bcaa4a8acbe5ba0449be5db23c607174f6131fc8
Michal Wozniak <mv740@users.noreply.github.com>
2019-02-13 22:41:01 -0500

    add simple message #2

--- slack.py
+++ slack.py
@@ -102,6 +102,22 @@ class SlackStatusPush(HttpStatusPushBase):
         builder_name = build['builder']['name']
         build_number = build['number']
 
+        slack_message = {
+                "channel" : "",
+                "attachments": [
+                    {
+                        "fallback": "%s - %s" % (state_message,build_url),
+                        "text": "<%s|%s # %s> - %s" %(build_url, builder_name, build_number,state_message),
+                        "color": self.BUILD_RESULT[statusToString(build['results'])]
+                    }
+                ]
+            }
+
+        # basic message for new event 
+        if event_name == 'new':
+            return slack_message
+
+        # build finished message
         build_properties = build['properties']
         build_commit_description = build_properties['commit-description']
         build_owner = build_properties['owner']
@@ -111,13 +127,7 @@ class SlackStatusPush(HttpStatusPushBase):
         Build_Version = build_properties.get('Build_Version', "Unknown")
 
         #https://api.slack.com/docs/messages#how_to_send_messages
-        slack_message = {
-                "channel" : "",
-                "attachments": [
-                    {
-                        "fallback": "%s - %s" % (state_message,build_url),
-                        "text": "<%s|%s # %s> - %s" %(build_url, builder_name, build_number,state_message),
-                        "fields": [
+        slack_message['attachments'][0]['fields'] = [
                             {
                                 "title": "Tag",
                                 "value": build_commit_description,
@@ -132,12 +142,7 @@ class SlackStatusPush(HttpStatusPushBase):
                                 "title": "Worker",
                                 "value": build_worker_name,
                                 "short": "true"
-                            }
-                        ],
-                        "color": self.BUILD_RESULT[statusToString(build['results'])]
-                    }
-                ]
-            }
+                            }]
 
         return slack_message
 
